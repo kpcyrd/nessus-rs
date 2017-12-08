9e94c2401301f05cd8c73bb2917dd4de30a8ee29
kpcyrd <git@rxv.cc>
2017-12-08 15:39:34 +0100

    Implement PartialEq

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -5,7 +5,7 @@ pub mod report;
 pub mod policy;
 
 /// Parsed nessus report
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub struct NessusClientDatav2 {
     #[serde(rename="Report")]
     pub report: report::Report,
